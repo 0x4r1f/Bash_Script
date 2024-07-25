8ac353b3ad234d256bf092eb28432f1a77d1b56d
Arif Hossen <arifhossen10100@gmail.com>
2024-07-25 20:13:03 +0600

    Checking the values of these environment variables

---
+++ env_Check.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "===Try checking the values of these environment variables:=== "
+
+echo ${HOSTNAME}
+echo ${OSTYPE}
+echo ${SHELL}
+echo ${BASHPID}
+echo ${BASH_VERSION}
+echo ${UID}
+echo ${GROUPS}
+
+bash --version
