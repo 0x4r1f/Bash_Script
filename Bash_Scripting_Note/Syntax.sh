c32d7f85fdc47814705ba80e15ada73862ce8a98
Arif Hossen <arifhossen10100@gmail.com>
2024-07-25 22:09:37 +0600

    About basic Bash Scripts

---
+++ Bash_Scripting_Note/Syntax.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+		mkdir mydirectory
+		touch mydirectory/myfile.txt
+		ls -l mydirectory
+
+
+# The most basic bash scripts are just lists of Linux commands collected in a single file. 
+	
+# 	=> For example
+
+# 		# All this script does is create a directory, create a file
+# 		# within the directory, and then list the content of the directory.
+
+# 		$mkdir mydirectory
+# 		$touch mydirectory/myfile.txt
+# 		$ls -l mydirectory
