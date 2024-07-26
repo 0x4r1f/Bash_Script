be67c95c7b67686286e98c64d26116cb58e78577
Arif Hossen <arifhossen10100@gmail.com>
2024-07-27 02:29:17 +0600

    Set, Delete, Swap, Updating, and Key-values

--- Bash_Scripting_Note/Arrays.sh
+++ Bash_Scripting_Note/Arrays.sh
@@ -4,10 +4,24 @@
 
 	# Set an array
 	IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
+
 	# Print all elements in the array
-	echo "${IP_ADDRESSES[*]}"
+	echo "All elements in the array : ${IP_ADDRESSES[*]}"
+
 	# Print only the first element in the array
-	echo "${IP_ADDRESSES[0]}"
+	echo "First element in the array : ${IP_ADDRESSES[0]}"
+
+	# Updating our array with new values 
+	IP_ADDRESSES+=( 0.0.0.0 8.8.8.8 )
+	echo "Updating values of new array are : ${IP_ADDRESSES[*]}"
+
+	# Key-values of Arrays
+	echo "Key-values of Arrays :- "
+	declare -A new_array
+	new_array=( [name]=Human [age]=25 [planet]=Earth)
+	echo "I am ${new_array[name]}"
+	echo "I live in ${new_array[planet]}"
+
 
 # 	* You can also delete elements from an array. The following will
 # 	delete 192.168.1.2 from the array:
